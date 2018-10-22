efb323f808b868060ff4650f4d25b9fe91456cfd
NathalieHerzog <43369868+NathalieHerzog@users.noreply.github.com>
2018-10-22 19:39:50 +0200

    Add files via upload

--- conditional.c
+++ conditional.c
@@ -1,88 +1,99 @@
-
 /*----------------------------------------------------------
  *				HTBLA-Leonding / Class: 2AHIF
  * ---------------------------------------------------------
  * Exercise Number: 4
  * Title:			  Conditional Arithmetics
  * Author:			Nathalie Herzog
- * Date:			17.10.
+ * Date:			22.10.
  * ----------------------------------------------------------
  * Description:
- * 
+ * The user chooses a number and an operator. The program
+ * calculates the result.
  * ----------------------------------------------------------
 */
-
+ 
 #include <stdio.h>
-
+ 
 void chooseOperation(int* operation)
 {
   do
   {
-  printf("What operatation should be performed? Add(1) or Multiply(2)\n");
+  printf("What operation should be performed? Add(1) or Multiply(2)\n");
   scanf("%d", operation);
   }while(*operation != 1 && *operation != 2);
 }
-
-void chooseNumber(int* number)
+ 
+void chooseNumber(int* range)
 {
   do
   {
   printf("Now select a number between 1 and 100\n");
-  scanf("%d", number);
-  }while(*number < 1 || *number > 100);
+  scanf("%d", range);
+  }while(*range < 1 || *range > 100);
 }
-
-void add(int number)
+ 
+void add(int range)
 {
-	long sum;
-
-	for(int i = 1; i < number; i++)
+	long result;
+ 
+	for(int i = 1; i < range; i++)
 	{
 		if(i % 5 == 0 || i % 3 == 0)
 		{
-			sum += i;
+			result += i;
 		}
-
+ 
 	}
-
-    printf("The result is: %ld\n", add);
+ 
+    if(result < 0)
+	{
+       printf("Overflow!\n");
+    }
+    else
+    {
+   		printf("The result is: %ld\n", result);
+    }
 }
-
-void multiply(int number)
+ 
+void multiply(int range)
 {
-	long multiply;
-
-	for(int i = 0; i < number; i++)
+	long result;
+ 
+	for(int i = 0; i < range; i++)
 	{
 		if(i % 5 == 0 || i % 3 == 0)
 		{
-			multiply *= i;
+			result *= i;
 		}
 	}
-
-    printf("The result is: %ld\n", multiply);
-    
+	 if(result < 0)
+	 {
+        printf("Overflow!\n");
+     }
+     else
+     {
+    	printf("The result is: %ld\n", result);
+     }
+ 
 }
-
-void performOperation(int operation, int number){
+ 
+void performOperation(int operation, int range){
   switch (operation)
   {
     case 1:
-      add(&number);
-      printf("Result: %d\n", add);
+      add(&range);
       break;
     case 2:
-      multiply(&number);
-
+      multiply(&range);
       break;
   }
 }
-
+ 
 int main(int argc, char const *argv[]) {
  int operation;
- int number;
-
+ int range;
+ 
 chooseOperation(&operation);
-chooseNumber(&number);
-performOperation(operation, number);
+chooseNumber(&range);
+performOperation(operation, range);
 }
